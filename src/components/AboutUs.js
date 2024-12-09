52d511b3faba604471c4d19b6cd1d04f3cbda706
pramod429-git <18kppramod@gmail.com>
2024-12-09 21:01:52 +0530

    class base changes

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,14 +1,46 @@
 import UserClass from "./UserClass";
-const AboutUs = () => {
-  return (
-    <div>
-      <h1>AboutUs</h1>
-      <UserClass
-        name={"Pramod Kumar K"}
-        location={"Mangalore"}
-        contact={"18kppramod@gmail.com"}
-      />
-    </div>
-  );
-};
+import { Component } from "react";
+
+class AboutUs extends Component {
+  constructor(props) {
+    super(props);
+    console.log("parent constuctor");
+  }
+  componentDidMount() {
+    console.log("parent component did mount");
+  }
+  render() {
+    console.log("parent render");
+    return (
+      <div>
+        <h1>AboutUs</h1>
+        <UserClass
+        // name={"child"}
+        // location={"Mangalore"}
+        // contact={"18kppramod@gmail.com"}
+        />
+
+        {/* <UserClass
+          name={"Child2"}
+          location={"Mangalore"}
+          contact={"18kppramod@gmail.com"}
+        /> */}
+      </div>
+    );
+  }
+}
 export default AboutUs;
+
+// const AboutUs = () => {
+//   return (
+//     <div>
+//       <h1>AboutUs</h1>
+//       <UserClass
+//         name={"Pramod Kumar K"}
+//         location={"Mangalore"}
+//         contact={"18kppramod@gmail.com"}
+//       />
+//     </div>
+//   );
+// };
+// export default AboutUs;
--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,4 +1,14 @@
+import { useEffect } from "react";
+
 const ContactUs = () => {
+  useEffect(() => {
+    const timmer = setInterval(() => {
+      console.log("call");
+    }, 1000);
+    return () => {
+      clearInterval(timmer);
+    };
+  }, []);
   return (
     <div>
       <h1>Contact-Us</h1>
